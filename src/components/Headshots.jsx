4e599a645c5e358f93942596e312c9390e29b27c
David Harsha <davishmcclurg@gmail.com>
2016-07-03 17:45:20 -0700

    add more headshots

--- src/components/Headshots.jsx
+++ src/components/Headshots.jsx
@@ -3,11 +3,20 @@
 import styles from 'styles/youtube.css'
 
 export default () => (
-  <div className={styles.youtubeContainer}>
-    <iframe
-      frameBorder="0"
-      allowFullScreen
-      src="https://www.youtube-nocookie.com/embed/Z3xd8bXlzeU?rel=0&controls=0&loop=1&showinfo=0"
-    />
+  <div>
+    <div className={styles.youtubeContainer}>
+      <iframe
+        frameBorder="0"
+        allowFullScreen
+        src="https://www.youtube-nocookie.com/embed/Z3xd8bXlzeU?rel=0&controls=0&loop=1&showinfo=0"
+      />
+    </div>
+    <div className={styles.youtubeContainer}>
+      <iframe
+        frameBorder="0"
+        allowFullScreen
+        src="https://www.youtube-nocookie.com/embed/Vd79lZaozFg?rel=0&controls=0&loop=1&showinfo=0"
+      />
+    </div>
   </div>
 )
