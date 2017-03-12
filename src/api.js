6b38e26f4659b3bd4c8856c5a8a4bd128592084e
David Harsha <davishmcclurg@gmail.com>
2017-03-12 15:47:55 -0700

    Remove function call for `previewable`

--- src/api.js
+++ src/api.js
@@ -1,11 +1,9 @@
 import Contentful from 'contentful'
 
-const previewable = () => (
-  ['staging.sofiewrites.com', 'localhost'].includes(window.location.hostname)
-)
+const previewable = ['staging.sofiewrites.com', 'localhost'].includes(window.location.hostname)
 
 export default Contentful.createClient({
-  host: previewable() ? 'preview.contentful.com' : 'cdn.contentful.com',
+  host: previewable ? 'preview.contentful.com' : 'cdn.contentful.com',
   space: '0maefx6qczs5',
   accessToken: '9770a44501473cc2ed7bb19c6cc5556cfbebbd3c40baaf03dce4f8e3aa880f7b',
 })
