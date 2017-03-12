75efc52dfaf8b6fda998696a9c93a85420ec0ed5
David Harsha <davishmcclurg@gmail.com>
2017-03-12 16:04:31 -0700

    Use non-preview access token in production

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 import Contentful from 'contentful'
 
 const previewable = ['staging.sofiewrites.com', 'localhost'].includes(window.location.hostname)
+const previewAccessToken = '9770a44501473cc2ed7bb19c6cc5556cfbebbd3c40baaf03dce4f8e3aa880f7b'
+const productionAccessToken = '3676e9941c4838e177ca479423a9792b6aa80ffce510f13eb2aabb93ee4cedae'
 
 export default Contentful.createClient({
   host: previewable ? 'preview.contentful.com' : 'cdn.contentful.com',
   space: '0maefx6qczs5',
-  accessToken: '9770a44501473cc2ed7bb19c6cc5556cfbebbd3c40baaf03dce4f8e3aa880f7b',
+  accessToken: previewable ? previewAccessToken : productionAccessToken,
 })
